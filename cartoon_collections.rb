764f858baf85552167987050d3f1244d4910bc29
Thompson Plyler <nealok.g@gmail.com>
2018-11-06 02:37:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,8 @@ def long_planeteer_calls(calls)
 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+
+  cheese.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
