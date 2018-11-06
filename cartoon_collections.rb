38b5826c1db52929c3912c9c70733a9756b78a46
Thompson Plyler <nealok.g@gmail.com>
2018-11-06 02:18:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,11 @@ puts dwarf_arr
 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)
+
+  planeteer.collect do |x|
+    x.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
