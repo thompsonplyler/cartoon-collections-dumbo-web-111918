de9f68ee0d652ae1352670c6bac8190876ddf02d
Thompson Plyler <nealok.g@gmail.com>
2018-11-06 03:29:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -49,17 +49,17 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese.any? do |x|
-cheese_types.include?(x)
-  end
-
-#  cheese.collect do |x|
-#    if cheese_types.include?(x)
-#      return x
-#    else
-#      nil
-#    end
+#if cheese.any? do |x|
+#cheese_types.include?(x)
 #  end
+
+  cheese.collect do |x|
+    cheese_types.find(x)
+      return x
+    else
+      nil
+    end
+  end
 
 
 end
