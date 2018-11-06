96d97de41669b9cca058b7f2f2458e2f74c4e1a0
Thompson Plyler <nealok.g@gmail.com>
2018-11-06 03:34:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,66 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+dwarf_arr = []
+
+  dwarves.each_with_index do |x, index|
+    dwarf_arr << "#{index+1}. #{x}"
+  end
+
+puts dwarf_arr
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)
+
+  planeteer.collect do |x|
+    x.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+
+  calls.collect do |x|
+    if x.length < 5
+      true
+    else
+      false
+    end
+  end.include?(false)
+
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# this successfull returns the cheese value if
+# there is an intersection, but it fails to
+# return nil
+#
+# def find_the_cheese(cheese)
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#
+#  if (cheese & cheese_types)
+#    (cheese & cheese_types).join
+#  else
+#    nil
+#  end
+#
+#end
+
+# end of failing method
+
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+#if cheese.any? do |x|
+#cheese_types.include?(x)
+#  end
+
+  cheese.find do |type|
+    cheese_types.include?(type)
+  end
+
+
 end
+
+
+# for any element of array:cheese, if it matches
+# array:cheese_types, return the value of the matched
+# cheese type.
